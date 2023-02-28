0b7a1ce88ad2d6a163b42ed7a75603c2c7b09e3d
abenezerayalew <aben.man.unity@gmail.com>
2023-02-28 16:56:44 +0300

    fix_betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,28 +1,33 @@
 #include "binary_trees.h"
-#include <stdlib.h>
-#include <stdio.h>
+/**
+ * binary_tree_insert_right - that inserts a node as the right-child of node.
+ *
+ * @parent: is a pointer to the node to insert the right-child in.
+ * @value:  is the value to store in the new node.
+ *
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{ 
-    binary_tree_t *new_node;
+{
+	binary_tree_t *new_node;
+	binary_tree_t *temp;
 
-    binary_tree_t *temp;
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
 
-    if (parent == NULL)
-        return NULL;
-    new_node = binary_tree_node(parent, value);
-    if (!new_node)
-        return NULL;
+	if (!new_node)
+		return (NULL);
 
-    if (parent->right)
-    {
-        temp = parent->right;
-        parent->right = new_node;
-        temp->parent = new_node;
-        new_node-> right = temp;
-    }
-    else
-        parent->right = binary_tree_node(parent, value);
-    
-    return new_node;
+	if (parent->right)
+	{
+		temp = parent->right;
+		parent->right = new_node;
+		temp->parent = new_node;
+		new_node->right = temp;
+	}
+	else
+		parent->right = new_node;
+	return (new_node);
 }
