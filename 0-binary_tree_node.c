38e142cbaa37145c6bafecfaa9279c579ea523e9
abenezerayalew <aben.man.unity@gmail.com>
2023-02-28 16:30:30 +0300

    fix_betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,15 +1,24 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * binary_tree_node - that creates a binary tree node.
+ *
+ * @parent: a pointer to the parent node of the node to create.
+ * @value: the value to put in the new node.
+ *
+ * Return: new_node
+ *
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t* new_node = malloc(sizeof(binary_tree_t));
-    if (new_node != NULL)
-    {
-        new_node->right = NULL;
-        new_node->left = NULL;
-        new_node->n = value;
-        new_node->parent =parent;
-    }
-    return new_node;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node != NULL)
+	{
+		new_node->right = NULL;
+		new_node->left = NULL;
+		new_node->n = value;
+		new_node->parent = parent;
+	}
+	return (new_node);
 }
