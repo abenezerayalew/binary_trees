a29c9d3dbe98d14633ba2c80c2a64665976fb305
abenezerayalew <aben.man.unity@gmail.com>
2023-03-01 17:07:41 +0300

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left_side = (tree->left) ? (binary_tree_height(tree->left)) : 0;
-		right_side = (tree->right) ? (binary_tree_height(tree->right)) : 0;
+		left_side = (tree->left) ? (1+binary_tree_height(tree->left)) : 0;
+		right_side = (tree->right) ? (1+binary_tree_height(tree->right)) : 0;
 		return (left_side > right_side ? left_side : right_side);
 	}
 	return (0);
